5211d489214d23cebea854403c95f85d901e73f8
brunosas88 <bruno.sas@live.com>
2022-10-29 14:12:01 -0300

    feat: executar a criação do cluster
    Adicionados scripts para windows(.bat) e
    linux(.sh) para facilitar a criação do cluster

---
+++ deploy-kubernetes-app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Criar imagens..."
+docker build -t brunosas88/app-php-backend:1.0 backend/.
+docker build -t brunosas88/app-php-database:1.0 database/.
+
+echo "Realizar push das imagens..."
+docker push brunosas88/app-php-backend:1.0
+docker push brunosas88/app-php-database:1.0
+
+echo "Criar serviços de conexão e load balancer..."
+kubectl apply -f ./services-lb-con.yml
+
+echo "Criar deployments..."
+kubectl apply -f ./deploy_pvc_db_app.yml
+
